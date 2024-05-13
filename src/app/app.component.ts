76c261e60196c34ecdbab2cd820e4ff2afac537a
newbeee1711 <ashritha.p60@gmail.com>
2024-05-14 00:14:27 +0530

    made changes to add matcard on button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+// import {FormsModule} from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { MatChipsModule} from '@angular/material/chips';
-import { FormGroup, FormControl } from '@angular/forms';
+// import { FormGroup, FormControl } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AbstractControl, FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +25,7 @@ export class AppComponent {
   title = 'gitAngularProject';
   chip:any;
   importantTasksSelected:any="";
+  addNotePad:boolean=false;
   
   frameworkChips = [ 
     { name: 'Do Laundry' }, 
@@ -29,12 +33,23 @@ export class AppComponent {
     { name: 'Get groceries' }, 
 ]; 
 //To be continued
+
+
 simpleForm = new FormGroup({
   taskArea: new FormControl(''),
   
   })
+  cards: any=[];
 
+  add(){
+    this.cards.push({});
+    this.addNotePad=true;
+  }
   Update(recievedChip:any){    
-    this.importantTasksSelected=recievedChip.name;
+    debugger;
+    this.importantTasksSelected=this.importantTasksSelected+recievedChip.name+", ";
+    console.log(this.importantTasksSelected);
+    this.simpleForm.controls.taskArea.setValue(this.importantTasksSelected);
+
   }
 }
